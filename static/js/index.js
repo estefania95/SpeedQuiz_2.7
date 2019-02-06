6b5fbd2a2b915dc353727a62dbf8e66ca888aa31
Xavi <xavierisachalemany@gmail.com>
2019-02-06 18:09:42 +0100

    Registre fet, falta retocar tamanys

---
+++ static/js/index.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#flexa--devant").on("click",function(){alert("hola mon")}),$("#id_username").attr("placeholder","Nom d'usuari"),$("#id_email").attr("placeholder","Email"),$("#id_password1").attr("placeholder","Contrasenya"),$("#id_password2").attr("placeholder","Repateix la constrasenya")});
