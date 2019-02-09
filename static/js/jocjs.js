7ee63bd5994d818f02b76d8393dffa6fa668943b
Xavi <xavierisachalemany@gmail.com>
2019-02-09 19:48:11 +0100

    optimitzacio de codi event click nomes es  fa un cop

--- static/js/jocjs.js
+++ static/js/jocjs.js
@@ -1,88 +1,72 @@
-var preguntes = 11;
-var pregunta = $('#pregunta');
-var r1 = $('#resposta--1');
-var r2 = $('#resposta--2');
-var r3 = $('#resposta--3');
-var r4 = $('#resposta--4');
+var preguntes = 10;
+var pregunta = $("#pregunta");
+var r1 = $("#resposta--1");
+var r2 = $("#resposta--2");
+var r3 = $("#resposta--3");
+var r4 = $("#resposta--4");
+var respostes = [r1, r2, r3, r4];
+var Response;
 var quesitos = 0;
-var clic = 0;
+var esCorrecte;
+eventsClick();
 
-function peticio(){
-    if (preguntes==1){
-        alert("Final Partida")
-    }
-    else{
-        $.ajax({
-            url: 'http://127.0.0.1:8000/home/api',
-            success: function (response) {
-
-            console.log(preguntes);
-               pregunta.html(response.textPregunta);
-
-               r1.html(response.respostes.resposta1.textResposta);
-               r2.html(response.respostes.resposta2.textResposta);
-               r3.html(response.respostes.resposta3.textResposta);
-               r4.html(response.respostes.resposta4.textResposta);
-
-
-               r1.on('click', function (){
-                    var boleano = response.respostes.resposta1.esCorrexta
-
-                        if(boleano){
-                            bloquearacierto();
-                        }
-                        else{
-                            bloquearerror();
-                        }
-                    }
-
-
-               });
-               r2.on('click', function (){
-                    var boleano2 = response.respostes.resposta2.esCorrexta
-                   if(boleano2){
-                        bloquearacierto();
-                    }
-                    else{
-                        bloquearerror();
-                    }
-               });
-               r3.on('click', function (){
-               var boleano3 = response.respostes.resposta3.esCorrexta
-                    if(boleano3){
-
-                        bloquearacierto();
-                    }
-                    else{
-
-                        bloquearerror();
-                    }
-               });
-               r4.on('click', function (boleano4){
-                    var boleano4 = response.respostes.resposta4.esCorrexta
-                    if(boleano4){
-
-                        bloquearacierto();
-                    }
-                    else{
-
-                        bloquearerror();
-                    }
-               });
-            },
-            error: function () {
-                alert("Error!");
+function peticio() {
+  if (preguntes == 0) {
+    alert("Final Partida");
+  } else {
+    $.ajax({
+      url: "http://127.0.0.1:8000/home/api",
+      success: function(response) {
+        Response = response;
+        inprimirContingut();
+      },
+      error: function() {
+        alert("Error!");
+      }
+    });
+  }
+}
+function eventsClick(){
+    var event = 
+[
+    Response.respostes.resposta1.esCorrexta,
+    Response.respostes.resposta2.esCorrexta, 
+    Response.respostes.respost3.esCorrexta,
+    Response.respostes.resposta4.esCorrexta
+]
+    for(let i=0; i< respostes.length; i++){
+        respostes[i].on("click", function() {
+            esCorrecte = event[i];
+            if (esCorrecte) {
+              bloquearacierto();
+              alert(esCorrecte);
+            } else {
+              bloquearerror();
+              alert(esCorrecte);
             }
-        });
+        });   
     }
+    respostes.forEach(r => {
+        console.log(r)
+        cont++;
+    });
 }
-function bloquearacierto(){
-    quesitos += 1 ;
-    preguntes -= 1;
 
+function inprimirContingut(){
+    console.log(Response)
+    pregunta.html(Response.textPregunta);
+
+    r1.html(Response.respostes.resposta1.textResposta);
+    r2.html(Response.respostes.resposta2.textResposta);
+    r3.html(Response.respostes.resposta3.textResposta);
+    r4.html(Response.respostes.resposta4.textResposta);
 }
 
-function bloquearerror(){
-    preguntes -= 1;
+function bloquearacierto() {
+  quesitos += 1;
+  preguntes -= 1;
+}
 
+function bloquearerror() {
+  preguntes -= 1;
 }
