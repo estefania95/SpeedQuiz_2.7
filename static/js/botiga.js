1f995cd3dfa034337450b648a093b8aef0a1f036
Estefania <estefi.95.c@gmail.com>
2019-02-10 19:54:03 +0100

    Mostrar missatge si no tens suficients formatges per comprar.

--- static/js/botiga.js
+++ static/js/botiga.js
@@ -1,15 +1,20 @@
-function accio(nomSkin, accio){
-    var url = "http://127.0.0.1:8000/home/botiga/";
-    var dades = {'nomSkin': nomSkin, 'accio': accio};
+function accio(nomSkin, accio, numFormatges, preuSkin){
+    if(accio=='comprar' && parseInt(numFormatges)<parseInt(preuSkin)){
+        alert("No tens formatgets suficients")
+    }
+    else{
+        var url = "http://127.0.0.1:8000/home/botiga/";
+        var dades = {'nomSkin': nomSkin, 'accio': accio};
 
-    $.ajax({
-          url: url,
-          type: 'GET',
-          contentType: 'application/json; charset=utf-8',
-          data: dades,
-          dataType: 'text',
-          success: function() {
+        $.ajax({
+              url: url,
+              type: 'GET',
+              contentType: 'application/json; charset=utf-8',
+              data: dades,
+              dataType: 'text',
+              success: function() {
 
-          }
-      });
+              }
+          });
+      }
 }
