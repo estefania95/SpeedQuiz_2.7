e05d3238b59d14f48c01fa5b1781efa216f5a3da
Xavi <xavierisachalemany@gmail.com>
2019-05-01 15:09:07 +0200

    servei puntuacio fet i classe puntuacio

--- static/speedQuiz/src/app/routes/puntuacio.service.ts
+++ static/speedQuiz/src/app/routes/puntuacio.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { DadesPuntuacio } from '../classes/dadesPuntuacio';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PuntuacioService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private url:string = 'https://localhost/api/puntuacio';
+
+  getDadesPuntuacio(): Observable<DadesPuntuacio[]> {
+    return this.http.get<DadesPuntuacio[]>(this.url);
+  }
 }
